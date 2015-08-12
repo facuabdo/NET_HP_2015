3eb5560d2b0d7363f31ab0061f67838c5b3851b7
Facundo Abdo <facuabdo@gmail.com>
2015-08-11 21:09:21 -0300

    Version 0.1.2

--- TuEntradaVirtual/Models/IdentityModels.cs
+++ TuEntradaVirtual/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace TuEntradaVirtual.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("TEVEntityModel", throwIfV1Schema: false)
         {
         }
 
